8151387b31a82d984a46c98fa9bcc3af468479b1
lakhdar <lkboudjahfa@gmail.com>
2024-03-07 00:13:04 +0100

    edit controller

--- src/lib/controller/propmt_controller.ts
+++ src/lib/controller/propmt_controller.ts
@@ -2,7 +2,36 @@ import prismaConfig from "../configs/prismaConfig";
 
 class PromptController {
   async showAll() {
-    return await prismaConfig.prompt.findMany({});
+    return await prismaConfig.prompt.findMany({
+      include: {
+        UsersLikerPrompts: true,
+        user: {
+          select: {
+            id: true,
+            name: true,
+            image: true,
+          },
+        },
+        technology: {
+          select: {
+            id: true,
+            name: true,
+            models: true,
+          },
+        },
+        comments: {
+          include: {
+            user: {
+              select: {
+                id: true,
+                name: true,
+                image: true,
+              },
+            },
+          },
+        },
+      },
+    });
   }
 }
 
